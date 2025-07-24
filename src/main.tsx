9fefc119fafc8ef5556197e327fc6e31e875afc6
JorgeRangel <jorgelrgarduno@gmail.com>
2025-07-24 15:09:26 -0600

    feat(router): configura React Router y layout base con Header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import AppRuter from './router'
 import './index.css'
-import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRuter />
   </StrictMode>,
 )
